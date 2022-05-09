0907e0e526244da17d6eda403a5adbfc4513cc6d
qq584037793 <65120534+qq584037793@users.noreply.github.com>
2022-05-09 15:32:04 +0900

    feat: route

---
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- 一级菜单 -->
+  <el-menu
+    :uniqueOpened="true"
+    default-active="2"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b"
+  >
+    <!-- 子菜单 -->
+    <el-submenu index="1">
+      <template #title>
+        <i class="el-icon-location"></i>
+        <span>导航栏</span>
+      </template>
+      <el-menu-item index="1-1">index1</el-menu-item>
+      <el-menu-item index="1-2">index2</el-menu-item>
+    </el-submenu>
+    <!-- 具体菜单 -->
+    <el-menu-item index="4">
+      <i class="el-icon-setting"></i>
+      <template #title>4</template>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useRouter } from 'vue-router'
+import { filterRouters, generateMenus } from '@/utils/route'
+
+const router = useRouter()
+const routes = computed(() => {
+  const filterRoutes = filterRouters(router.getRoutes())
+  return generateMenus(filterRoutes)
+})
+console.log(JSON.stringify(routes.value))
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/layout/components/Sidebar/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>111</h1>
+    <el-scrollbar>
+      <sidebar-menu></sidebar-menu>
+    </el-scrollbar>
+  </div>
+</template>
+
+<script setup>
+import {} from 'vue'
+import SidebarMenu from './SidebarMenu'
+
+</script>
+<style lang="scss" scoped></style>
